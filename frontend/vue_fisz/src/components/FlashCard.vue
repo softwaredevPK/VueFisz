6185bf685549527e4a8771562697908cc5d8a617
softwaredevPK <pskotarba@gmail.com>
2022-05-31 23:40:27 +0200

    FlashCard moved as new component. ViewFlashCards started to using new component by props and emits

---
+++ frontend/vue_fisz/src/components/FlashCard.vue
@@ -0,0 +1,143 @@
+<template>
+	<label>
+	<input type="checkbox" />
+		<div class="flip-card">
+			<div class="front">
+				<p>{{frontText}}</p>
+			</div>
+			<div class="back">
+                <p>{{backText}}</p>
+			</div>
+		</div>
+        <div class="actions">
+            <button @click="emit('learned')">Nauczone</button>
+            <button @click="emit('toBeRepeated')">Do powtórki</button>
+            <button @click="emit('next')">Next</button>
+        </div>
+	</label>
+    
+</template>
+		
+<script setup>
+    const props = defineProps(['frontText', 'backText'])
+    const emit = defineEmits(['next', "learned", "toBeRepeated"])  // states of flashcard
+</script>
+
+<style scoped>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+label {
+	display: block;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	width: 280px;
+	height: 350px;
+	perspective: 1000px;
+	transform-style: preserve-3d;
+	transform: translate(-50%, -50%);
+	cursor: pointer;
+}
+
+.flip-card {
+	position: relative;
+	width: 100%;
+	height: 100%;
+	transform-style: preserve-3d;
+	transition: all 0.5s ease-in-out;
+	z-index: 1;
+}
+
+.flip-card .front,
+.flip-card .back {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	text-align: center;
+	background: #fff;
+	backface-visibility: hidden;
+	border-radius: 0 20px 0 20px;
+}
+
+.flip-card .back {
+	transform: rotateY(180deg);
+	color: #000;
+	background: #fff;
+}
+
+label:hover .flip-card {
+	transform: rotateY(2deg);
+	box-shadow: 0 20px 20px rgba(50, 60, 60, 0.2);
+}
+
+input {
+	display: none;
+}
+
+:checked + .flip-card {
+	transform: rotateY(180deg);
+}
+label:hover :checked + .flip-card {
+	transform: rotateY(175deg);
+	box-shadow: 0 20px 20px rgba(255, 255, 255, 0.2);
+}
+
+.front img {
+	width: 110px;
+	height: 110px;
+	margin: 30px 0 20px 0;
+	border-radius: 50%;
+}
+
+.front h1 {
+	font-size: 30px;
+	color: #499bef;
+	margin: 0;
+}
+.front h2 {
+	font-size: 18px;
+	color: #4e699d;
+	margin: 12px 0 12px 0;
+}
+
+.front b {
+	font-size: 14px;
+	color: #424752;
+	margin: 0 0 35px 0;
+	display: block;
+}
+
+.front p,
+.back .click {
+	font-size: 18px;
+	font-weight: 600;
+}
+
+.back h1 {
+	color: #424752;
+	margin: 30px 0 0 0;
+}
+
+hr {
+	width: 180px;
+	margin: 15px auto 10px auto;
+}
+
+.back p {
+	font-size: 16px;
+	color: #000;
+	padding: 0 18px;
+	line-height: 30px;
+	text-align: center;
+	margin: 0 auto;
+}
+
+</style>
+
+
+
+
--- frontend/vue_fisz/src/views/ViewFlashCards.vue
+++ frontend/vue_fisz/src/views/ViewFlashCards.vue
@@ -1,144 +1,19 @@
 <template>
-	<label>
-	<input type="checkbox" />
-		<div class="flip-card">
-			<div class="front">
-				<h1>Set {{ $route.params.set_id }}</h1>
-				<img src="@/assets/Logo.png">
-				<p>Click to flip</p>
-			</div>
-			<div class="back">
-				<h1>About me</h1>
-				<hr />
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since forever.</p>
-				<hr />
-				<p class="click">Click to flip</p>
-			</div>
-		</div>
-	</label>
+	test
+	<FlashCard :frontText="frontText" :backText="backText" @next="next"></FlashCard>
 </template>
 		
-<script>
-export default {
-    name: 'ViewFlashCards',
+<script setup>
+import FlashCard from '@/components/FlashCard.vue';
+import {ref} from 'vue'
+
+let frontText = ref('test')
+let backText = ref('back test')
+
+function next() {
+	frontText.value = 'Tutaj powinno sie zmieniac w funkcji'
+	backText.value = 'tutaj tez powinno sie zmieniac, ta funkcja dynamicznie zmienia'
 }
 </script>
-
-<style scoped>
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
-
-label {
-	display: block;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	width: 280px;
-	height: 350px;
-	perspective: 1000px;
-	transform-style: preserve-3d;
-	transform: translate(-50%, -50%);
-	cursor: pointer;
-}
-
-.flip-card {
-	position: relative;
-	width: 100%;
-	height: 100%;
-	transform-style: preserve-3d;
-	transition: all 0.5s ease-in-out;
-	z-index: 1;
-}
-
-.flip-card .front,
-.flip-card .back {
-	position: absolute;
-	width: 100%;
-	height: 100%;
-	text-align: center;
-	background: #fff;
-	backface-visibility: hidden;
-	border-radius: 0 20px 0 20px;
-}
-
-.flip-card .back {
-	transform: rotateY(180deg);
-	color: #000;
-	background: #fff;
-}
-
-label:hover .flip-card {
-	transform: rotateY(2deg);
-	box-shadow: 0 20px 20px rgba(50, 60, 60, 0.2);
-}
-
-input {
-	display: none;
-}
-
-:checked + .flip-card {
-	transform: rotateY(180deg);
-}
-label:hover :checked + .flip-card {
-	transform: rotateY(175deg);
-	box-shadow: 0 20px 20px rgba(255, 255, 255, 0.2);
-}
-
-.front img {
-	width: 110px;
-	height: 110px;
-	margin: 30px 0 20px 0;
-	border-radius: 50%;
-}
-
-.front h1 {
-	font-size: 30px;
-	color: #499bef;
-	margin: 0;
-}
-.front h2 {
-	font-size: 18px;
-	color: #4e699d;
-	margin: 12px 0 12px 0;
-}
-
-.front b {
-	font-size: 14px;
-	color: #424752;
-	margin: 0 0 35px 0;
-	display: block;
-}
-
-.front p,
-.back .click {
-	font-size: 18px;
-	font-weight: 600;
-}
-
-.back h1 {
-	color: #424752;
-	margin: 30px 0 0 0;
-}
-
-hr {
-	width: 180px;
-	margin: 15px auto 10px auto;
-}
-
-.back p {
-	font-size: 16px;
-	color: #000;
-	padding: 0 18px;
-	line-height: 30px;
-	text-align: center;
-	margin: 0 auto;
-}
-
-</style>
-
-
 
 
