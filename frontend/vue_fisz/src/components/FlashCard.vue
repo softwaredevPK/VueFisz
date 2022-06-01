f49df9e6f397058d7d3965cb739ee576288d03d1
radek024 <radek002@o2.pl>
2022-06-01 18:46:13 +0200

    flashcard redesign

--- frontend/vue_fisz/src/components/FlashCard.vue
+++ frontend/vue_fisz/src/components/FlashCard.vue
@@ -9,10 +9,10 @@
                 <p>{{backText}}</p>
 			</div>
 		</div>
-        <div class="actions">
-            <button @click="emit('learned')">Nauczone</button>
-            <button @click="emit('toBeRepeated')">Do powtórki</button>
-            <button @click="emit('next')">Next</button>
+        <div class="btn-actions">
+            <button class="btn-learned" @click="emit('learned')">Nauczone</button>
+            <button class="btn-toRepeat" @click="emit('toBeRepeated')">Do powtórki</button>
+            <button class="btn-next" @click="emit('next')">Next</button>
         </div>
 	</label>
     
@@ -32,14 +32,10 @@
 
 label {
 	display: block;
-	position: absolute;
-	top: 50%;
-	left: 50%;
 	width: 280px;
 	height: 350px;
 	perspective: 1000px;
 	transform-style: preserve-3d;
-	transform: translate(-50%, -50%);
 	cursor: pointer;
 }
 
@@ -60,18 +56,25 @@ label {
 	text-align: center;
 	/* background: #fff; */
 	backface-visibility: hidden;
-	border-radius: 0 20px 0 20px;
+	border-radius: 20px;
+	border: 1px solid #c1c1c1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	vertical-align: middle;
+	font-size: 22px;
+	font-weight: 600;
 }
 
 .flip-card .back {
 	transform: rotateY(180deg);
-	color: #000;
+	color: #FFF;
+	background-color: #000;
 	/* background: #fff; */
 }
 
 label:hover .flip-card {
 	transform: rotateY(2deg);
-	box-shadow: 0 20px 20px rgba(50, 60, 60, 0.2);
 }
 
 input {
@@ -86,62 +89,33 @@ label:hover :checked + .flip-card {
 	box-shadow: 0 20px 20px rgba(255, 255, 255, 0.2);
 }
 
-.front {
-    background-color: grey;
-}
-
-.back {
-    background-color: grey;
-}
-
-.front img {
-	width: 110px;
-	height: 110px;
-	margin: 30px 0 20px 0;
-	border-radius: 50%;
-}
-
-.front h1 {
-	font-size: 30px;
-	color: #499bef;
-	margin: 0;
-}
-.front h2 {
-	font-size: 18px;
-	color: #4e699d;
-	margin: 12px 0 12px 0;
-}
-
-.front b {
-	font-size: 14px;
-	color: #424752;
-	margin: 0 0 35px 0;
-	display: block;
-}
-
-.front p,
-.back .click {
-	font-size: 18px;
-	font-weight: 600;
-}
-
-.back h1 {
-	color: #424752;
-	margin: 30px 0 0 0;
-}
-
 hr {
 	width: 180px;
 	margin: 15px auto 10px auto;
 }
 
-.back p {
-	font-size: 16px;
-	color: #000;
-	padding: 0 18px;
-	line-height: 30px;
-	text-align: center;
-	margin: 0 auto;
+.btn-actions{
+	display: flex;
+    flex-direction: column;
+    margin: 20px auto;
+    max-width: 80%;
+    justify-content: center;
+}
+
+.btn-actions [class*="btn"]{
+	background: #FFF;
+	border: 1px solid #c1c1c1;
+	border-radius: 12px;
+	padding: 10px;
+	font-weight: 700;
+	margin-top: 10px;
+}
+
+.btn-actions [class*="btn"]:hover,
+.btn-actions [class*="btn"]:focus{ 
+	background: #000;
+	color: #FFF;
+	cursor: pointer;
 }
 
 </style>
